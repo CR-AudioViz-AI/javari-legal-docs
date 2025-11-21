93e86dfb636a295a93092e097b6587ba00d2165f
Old <225056366+roy-henderson@users.noreply.github.com>
2025-11-21 15:07:36 -0500

    Exclude supabase/functions from Next.js build

--- next.config.js
+++ next.config.js
@@ -5,7 +5,14 @@ const nextConfig = {
   images: {
     domains: ['kteobfyferrukqeolofj.supabase.co'],
   },
-  // Remove experimental serverActions as it's stable in Next.js 14
+  // Exclude Supabase Edge Functions from Next.js build
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      exclude: /supabase[\\/]functions/,
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
